c2fdb95862300db81e08cf0c6be4694c95611f2a
Rickard Bemm <rickos99@hotmail.com>
2019-11-21 16:58:40 +0100

    Update bSort.java
    The sorting algorithm is now working as expected

--- src/lab1/bSort.java
+++ src/lab1/bSort.java
@@ -1,64 +1,45 @@
 package lab1;
 
-import java.util.Arrays;
-
-import profiling.ArraysGenerator;
-
 public class bSort {
 
 	/**
 	 * Search for a integer in a given array.
-	 * (<a href="https://www.geeksforgeeks.org/binary-search/">Source</a>)
 	 * 
-	 * @param arr array to search
-	 * @param l   left boundary
-	 * @param r   right boundary
-	 * @param x   integer to find index of in array
+	 * @param arr  array to search
+	 * @param low  left boundary
+	 * @param high right boundary
+	 * @param key  integer to find index of in array
 	 * @return index of value in arr, if not found -1
 	 */
-	private static int binarySearch(int[] arr, int l, int r, int x) {
-		if (r >= l) {
-			int mid = l + (r - l) / 2;
+	private static int binarySearch(int[] arr, int low, int high, int key) {
+		int mid;
 
-			// If the element is present at the
-			// middle itself
-			if (arr[mid] == x)
-				return mid;
-
-			// If element is smaller than mid, then
-			// it can only be present in left subarray
-			if (arr[mid] > x)
-				return binarySearch(arr, l, mid - 1, x);
-
-			// Else the element can only be present
-			// in right subarray
-			return binarySearch(arr, mid + 1, r, x);
+		if (low == high) {
+			return low;
 		}
 
-		// We reach here when element is not present
-		// in array
-		return -1;
-	}
+		mid = low + ((high - low) / 2);
 
-	public static void main(String[] args) {
-//		int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 67, 89, 90, 91, 92, 99, 100 };
-//		System.out.println(binarySearch(arr, 0, arr.length, 67));
-		int[] arr = new ArraysGenerator().randomArray(5);
-		System.out.println(Arrays.toString(sort(arr)));
+		if (key > arr[mid]) {
+			return binarySearch(arr, mid + 1, high, key);
+		} else if (key < arr[mid]) {
+			return binarySearch(arr, low, mid, key);
+		}
+
+		return mid;
 	}
 
 	public static int[] sort(int[] input) {
 		int n = input.length;
-		for (int i = 0; i < n; ++i) {
+		for (int i = 1; i < n; ++i) {
 			int key = input[i];
 			int index = binarySearch(input, 0, i, key);
-			
-//			for (int j = i; j > index; j--) {
-//				input[j + 1] = input[j];
-//			}
-//			input[index] = key;
-//			Shift all elements in array
-//			Current state: not working!
+
+			for (int j = i - 1; j >= index; j--) {
+				input[j + 1] = input[j];
+			}
+
+			input[index] = key;
 		}
 
 		return input;

a0fbd7aaa3214d9f7ca16ae25a0a40efbb63bc7e
Rickard Bemm <rickos99@hotmail.com>
2019-11-22 15:02:25 +0100

    Update ArraysGenerator.java
    ## Added
    - Ablility to generate partly sorted and sorted arrays

--- src/profiling/ArraysGenerator.java
+++ src/profiling/ArraysGenerator.java
@@ -3,6 +3,9 @@ package profiling;
 import java.util.Arrays;
 
 public class ArraysGenerator {
+
+	final int numberSize = 100000;
+
 	public void print(int[] arr) {
 		System.out.println(Arrays.toString(arr));
 	}
@@ -10,8 +13,33 @@ public class ArraysGenerator {
 	public int[] randomArray(int elements) {
 		int[] arr = new int[elements];
 		for (int i = 0; i < elements; i++) {
-			arr[i] = (int) (Math.random() * 100000);
+			arr[i] = (int) (Math.random() * numberSize);
 		}
+		return arr;
+	}
+
+	public int[] randomKSorted(int elements, int sortedParts) {
+		int[] resultArr = new int[elements];
+		int elementsInEachPart = Math.round(elements / sortedParts);
+		int index = 0;
+
+		for (int i = 0; i < sortedParts; i++) {
+			int[] arr = this.randomSorted(elementsInEachPart);
+			for (int value : arr) {
+				resultArr[index++] = value;
+			}
+		}
+
+		// Fill empty spots in result array with random numbers
+		while (index < elements) {
+			resultArr[index++] = (int) (Math.random() * numberSize);
+		}
+		return resultArr;
+	}
+	
+	public int[] randomSorted(int elements) {
+		int[] arr = randomArray(elements);
+		Arrays.sort(arr);
 		return arr;
 	}
 }

026ad2238a44b6e8e7fda5601eaa4b165d2e3202
XKillalot <XKillalot@gmail.com>
2019-11-23 22:35:15 +0100

    Merging 2 arrays works now
    Can now merge 2 arrays through merge function, needs to loop through to merge all subarrays now

--- src/lab1/MergeSort.java
+++ src/lab1/MergeSort.java
@@ -4,9 +4,46 @@ import java.util.*;
 
 public class MergeSort {
 	public static void main(String[] args){
-		int[] input = {2, 1, 4, 3, 7, 6, 4, 2, 3, 4, 3};
+		int[] input = {2, 4, 6};
+		int[] input2= {1, 3, 5};
+		int[] sorted = merge(input, input2);
+		System.out.println(sorted.length);
+		for (int i = 0; i < sorted.length; i++) {
+			System.out.println(sorted[i]);
+		}
 		int k = 2;
-		sort(input, k);
+	}
+	public static int[] merge(int[] input1, int[] input2) {
+		
+		int[] sorted = new int[input1.length+input2.length];
+		int i=0,j=0,k=0;
+		
+		while(i < input1.length && j < input2.length) {
+			
+			if (input1[i] <= input2[j]) {
+				
+				sorted[k] = input1[i];
+				i++;
+			}
+			else {
+				
+				sorted[k] = input2[j];
+				j++;
+			}
+			k++;
+		}
+		
+		while(i < input1.length) {
+			sorted[k] = input1[i];
+			i++;
+			k++;
+		}
+		while(k < input2.length) {
+			sorted[k] = input2[j];
+			j++;
+			k++;
+		}
+		return sorted;
 	}
 	public static int[] sort(int[] input, int k) {
 
@@ -18,14 +55,14 @@ public class MergeSort {
 		//Delar upp så många delar den kan i k stora subarrays som läggs in i
 		// en array av arrays
 		for (int i = 0; i <= len - k; i += k) {
-		    newArray[counter++] = Arrays.copyOfRange(input, i, i + k);
+		    newArray[counter++] = InsertionSort.sort(Arrays.copyOfRange(input, i, i + k));
 		}
 		
 		//Om vi har rest så lägger vi till den i en mindre subarray och lägger in
 		// i array-arrayen
 		if (len % k != 0)
 			System.out.println(counter);
-		    newArray[counter] = Arrays.copyOfRange(input, len - len % k, len - 1);
+		    newArray[counter] = InsertionSort.sort(Arrays.copyOfRange(input, len - len % k, len - 1));
 		
 //		for(int j = 0; j <= len/k ; j++) {
 //			System.out.println("j = " + j);
@@ -34,9 +71,6 @@ public class MergeSort {
 //			}
 //			
 //		}
-		
-		
-		return input;
 	}
 // källa för delar av koden https://stackoverflow.com/questions/19237371/split-array-into-pieces-of-x-length
 }

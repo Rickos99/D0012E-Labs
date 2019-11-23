c05ba7db5b4a8e25bb6c1eca46ada0be9d122521
XKillalot <XKillalot@gmail.com>
2019-11-23 18:59:55 +0100

    Update MergeSort.java
    should divide correctly now

--- src/lab1/MergeSort.java
+++ src/lab1/MergeSort.java
@@ -4,29 +4,39 @@ import java.util.*;
 
 public class MergeSort {
 	public static void main(String[] args){
-		int[] input = {2, 1, 4, 3, 7, 6, 4, 2, 3, 2, 3};
+		int[] input = {2, 1, 4, 3, 7, 6, 4, 2, 3, 4, 3};
 		int k = 2;
 		sort(input, k);
 	}
 	public static int[] sort(int[] input, int k) {
 
 		int len = input.length;
+		System.out.println(len);
 		int counter = 0;
-		int[][] newArray = new int[len/k][k];
-		for (int i = 0; i < len - k + 1; i += k)
-		    newArray[counter++] = Arrays.copyOfRange(input, i, i + k);
-
-		if (len % k != 0)
-		    newArray[counter] = Arrays.copyOfRange(input, len - len % k, len);
+		int[][] newArray = new int[len/k + 1][k];
 		
-		for(int j = 0; j < len/k; j++) {
-			for(int l = 0; l < k; l++) {
-				System.out.print(newArray[j][l]);
-			}
-			System.out.print(" ");
+		//Delar upp så många delar den kan i k stora subarrays som läggs in i
+		// en array av arrays
+		for (int i = 0; i <= len - k; i += k) {
+		    newArray[counter++] = Arrays.copyOfRange(input, i, i + k);
 		}
+		
+		//Om vi har rest så lägger vi till den i en mindre subarray och lägger in
+		// i array-arrayen
+		if (len % k != 0)
+			System.out.println(counter);
+		    newArray[counter] = Arrays.copyOfRange(input, len - len % k, len - 1);
+		
+//		for(int j = 0; j <= len/k ; j++) {
+//			System.out.println("j = " + j);
+//			for(int l = 0; l < k; l++) {
+//				System.out.println(newArray[j][l]);
+//			}
+//			
+//		}
 		
 		
 		return input;
 	}
+// källa för delar av koden https://stackoverflow.com/questions/19237371/split-array-into-pieces-of-x-length
 }

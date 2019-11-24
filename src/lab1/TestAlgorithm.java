72f9f15c49a92948a99bb1675dfc44cb0e34df64
Rickard Bemm <rickos99@hotmail.com>
2019-11-24 16:04:33 +0100

    Update TestAlgorithm.java

--- src/lab1/TestAlgorithm.java
+++ src/lab1/TestAlgorithm.java
@@ -1,6 +1,7 @@
 package lab1;
 
 import java.util.Arrays;
+import java.util.Iterator;
 
 import profiling.ArraysGenerator;
 import profiling.TimeMeasure;
@@ -8,49 +9,90 @@ import profiling.TimeMeasure;
 public class TestAlgorithm {
 
 	public static void main(String[] args) {
-//		testMergeAlgorithms();
-		testSortAlgorithms();
+		final int tests = 10;
+		final int subArrayLength = 5;
+		final int[] dataLengths = {10,100,1000,10000,50000, 100000};
+		
+		testMergeAlgorithms(tests, dataLengths, subArrayLength);
+		testSortAlgorithms(tests, dataLengths);
 	}
 	
-	public static void testMergeAlgorithms() {
-		
-	}
-
-	public static void testSortAlgorithms() {
+	public static void testMergeAlgorithms(int tests, int[] dataLengths, int subArrayLength) {
 		TimeMeasure timer = new TimeMeasure();
 		ArraysGenerator arrayGenerator = new ArraysGenerator();
 		
-		final int tests = 100;
-		final int dataLength = 80000;
-		
-		double[] execInsertionSort = new double[tests];
-		double[] execBSort = new double[tests];
+		for (int dataLength : dataLengths) {
+			double[] execInsertionSort = new double[tests];
+			double[] execBSort = new double[tests];
 
-		for (int i = 0; i < tests; i++) {
-			final int[] arr = arrayGenerator.randomArray(dataLength);
-			int[] arrCopy;
-			
-			arrCopy = Arrays.copyOf(arr, arr.length);
-			timer.reset();
-			
-			// Test insertionSort and measure execution time
-			timer.start();
-			InsertionSort.sort(arrCopy);
-			timer.stop();
-			execInsertionSort[i] = timer.getMilliSeconds();
-			
-			timer.reset();
-			arrCopy = Arrays.copyOf(arr, arr.length);
+			for (int i = 0; i < tests; i++) {
+				final int[] arr = arrayGenerator.randomArray(dataLength);
+				int[] arrCopy;
+				
+				arrCopy = Arrays.copyOf(arr, arr.length);
+				timer.reset();
+				
+				// Test insertionSort and measure execution time
+				timer.start();
+				MergeSort.sort(arrCopy, subArrayLength);
+				timer.stop();
+				execInsertionSort[i] = timer.getMilliSeconds();
+				
+				timer.reset();
+				arrCopy = Arrays.copyOf(arr, arr.length);
 
-			// Test bSort and measure execution time
-			timer.start();
-			bSort.sort(arrCopy);
-			timer.stop();
-			execBSort[i] = timer.getMilliSeconds();
+				// Test bSort and measure execution time
+				timer.start();
+				MergebSort.sort(arrCopy, subArrayLength);
+				timer.stop();
+				execBSort[i] = timer.getMilliSeconds();
+			}
+
+			System.out.println("New Test");
+			System.out.println("n:" + dataLength +  ", k:" + subArrayLength);
+			System.out.println("Merge insertionSort: " + roundTwoDecimals(average(execInsertionSort)) + " ms");
+			System.out.println("Merge bSort:         " + roundTwoDecimals(average(execBSort)) + " ms");
+			System.out.println("");
 		}
+	}
 
-		System.out.println("insertionSort: " + roundTwoDecimals(average(execInsertionSort)) + " ms");
-		System.out.println("bSort: " + roundTwoDecimals(average(execBSort)) + " ms");
+	public static void testSortAlgorithms(int tests, int[] dataLengths) {
+		TimeMeasure timer = new TimeMeasure();
+		ArraysGenerator arrayGenerator = new ArraysGenerator();
+		
+		for (int dataLength : dataLengths) {
+			double[] execInsertionSort = new double[tests];
+			double[] execBSort = new double[tests];
+
+			for (int i = 0; i < tests; i++) {
+				final int[] arr = arrayGenerator.randomArray(dataLength);
+				int[] arrCopy;
+				
+				arrCopy = Arrays.copyOf(arr, arr.length);
+				timer.reset();
+				
+				// Test insertionSort and measure execution time
+				timer.start();
+				InsertionSort.sort(arrCopy);
+				timer.stop();
+				execInsertionSort[i] = timer.getMilliSeconds();
+				
+				timer.reset();
+				arrCopy = Arrays.copyOf(arr, arr.length);
+
+				// Test bSort and measure execution time
+				timer.start();
+				bSort.sort(arrCopy);
+				timer.stop();
+				execBSort[i] = timer.getMilliSeconds();
+			}
+
+			System.out.println("New Test");
+			System.out.println("n:" + dataLength);
+			System.out.println("insertionSort: " + roundTwoDecimals(average(execInsertionSort)) + " ms");
+			System.out.println("bSort:         " + roundTwoDecimals(average(execBSort)) + " ms");
+			System.out.println("");
+		}
 	}
 	
 	private static double roundTwoDecimals(double value) {

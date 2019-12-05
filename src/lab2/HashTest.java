b757bc0fa2c03a1d3720b17a2a656a4977540ec5
XKillalot <XKillalot@gmail.com>
2019-12-05 18:15:48 +0100

    Started working on implementation,
    Method used inspired by https://www.sanfoundry.com/java-program-implement-hash-tables-linear-probing/

---
+++ src/lab2/HashTest.java
@@ -0,0 +1,8 @@
+package lab2;
+
+class HashTest {
+	
+	public static void main(String[] args) {
+		LinearHash hashtable = new LinearHash(10);
+	}
+}
--- src/lab2/LinearHash.java
+++ src/lab2/LinearHash.java
@@ -1,6 +1,37 @@
 package lab2;
 
-public class LinearHash {
-
+class LinearHash {
+	private int currentSize, maxSize;
+	private int[] keys;
+	private int[] offset;
 	
+	public LinearHash(int capacity) {
+		currentSize = 0;
+		maxSize = capacity;
+		keys = new int[maxSize];
+		offset = new int[maxSize];
+	}
+	private int hash(int key) {
+		return key % maxSize;
+	}
+	public void insert(int key) {
+		int tmp = hash(key);
+		int i = 0;
+		int home;
+		do {
+			home = (tmp + i)%maxSize;
+			if(keys[home] == 0) {
+				keys[home] = key;
+				offset[home] = i;
+				currentSize++;
+				return;
+			}
+			if(keys[home] == key) {
+				offset[home] = i;
+				return;
+			}
+			i = i + 1;
+		}while(i + tmp != maxSize);
+		
+	}
 }

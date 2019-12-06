a62d62160aee6b553fe917fc9cd02a807db27fcd
Rickard Bemm <rickos99@hotmail.com>
2019-12-06 10:06:58 +0100

    Update LinearHash.java
    Added missing new line after each key in hash table

--- src/lab2/LinearHash.java
+++ src/lab2/LinearHash.java
@@ -54,7 +54,7 @@ class LinearHash {
     	String res = "Content in HashTable\n";
     	for (int i = 0; i < keys.length; i++) {
 			int key = keys[i];
-			res += "[" + i +"] : " + key;
+			res += "[" + i +"] : " + key + "\n";
 		}
     	return res;
     }

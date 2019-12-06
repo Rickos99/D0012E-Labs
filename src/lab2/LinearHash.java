20118169346df47857ff71e5291cc23f85455538
Rickard Bemm <rickos99@hotmail.com>
2019-12-06 10:00:12 +0100

    Update LinearHash.java
    ## Added
     - toString method to output all keys in hash table.

--- src/lab2/LinearHash.java
+++ src/lab2/LinearHash.java
@@ -48,4 +48,14 @@ class LinearHash {
     public void search(int key){
         throw new UnsupportedOperationException("The method LinearHash.search has not been implmented yet");
     }
+    
+    @Override
+    public String toString() {
+    	String res = "Content in HashTable\n";
+    	for (int i = 0; i < keys.length; i++) {
+			int key = keys[i];
+			res += "[" + i +"] : " + key;
+		}
+    	return res;
+    }
 }
